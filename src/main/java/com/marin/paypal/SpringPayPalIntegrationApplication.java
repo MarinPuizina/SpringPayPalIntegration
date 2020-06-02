de3a18cdbf7e25e1e8da83b84fbdcdd94c00164e
MarinPuizina <marinpuizina@hotmail.com>
2020-06-02 21:11:22 +0200

    Add initial commit

---
+++ src/main/java/com/marin/paypal/SpringPayPalIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.marin.paypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPayPalIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPayPalIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marin/paypal/SpringPayPalIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marin.paypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPayPalIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
