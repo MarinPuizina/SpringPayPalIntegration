7af6c075832fe04832c43d7fc0c969301731e99e
MarinPuizina <marinpuizina@hotmail.com>
2020-06-03 20:38:34 +0200

    Add paypalSdkConfig method

--- src/main/java/com/marin/paypal/PayPalConfig.java
+++ src/main/java/com/marin/paypal/PayPalConfig.java
@@ -1,7 +1,11 @@
 package com.marin.paypal;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class PayPalConfig {
@@ -14,5 +18,14 @@ public class PayPalConfig {
 
     @Value("${paypal.client.secret}")
     private String clientSecret;
+
+    @Bean
+    public Map<String, String> paypalSdkConfig() {
+
+        Map<String, String> configMap = new HashMap<>();
+        configMap.put("mode", mode);
+
+        return configMap;
+    }
 
 }
