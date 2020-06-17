f7c2650c497fbc233c62f20dce8e8d92e81e4a10
MarinPuizina <marinpuizina@hotmail.com>
2020-06-17 23:13:55 +0200

    Edit Order class

--- src/main/java/com/marin/paypal/Order.java
+++ src/main/java/com/marin/paypal/Order.java
@@ -1,4 +1,19 @@
 package com.marin.paypal;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Order {
+
+    private String price;
+    private String method;
+    private String intent;
+    private String description;
+
 }
